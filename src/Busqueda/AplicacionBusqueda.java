6e1f2877a8ae7a82208dba59d46e753b2163d48d
Daniel Asanza Erazo <danielprogramando2000@gmail.com>
2023-06-18 20:54:36 -0500

    Cambios en modulo de busqueda

--- src/Busqueda/AplicacionBusqueda.java
+++ src/Busqueda/AplicacionBusqueda.java
@@ -1,13 +1,183 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Busqueda;
 
-/**
- *
- * @author danny
- */
+import java.util.Arrays;
+import java.util.Scanner;
+
+
 public class AplicacionBusqueda {
     
+    private int[] numeros;
+    private int tamaño;
+    private Scanner scanner;
+
+    public AplicacionBusqueda(int capacidad) {
+        numeros = new int[capacidad];
+        tamaño = 0;
+        scanner = new Scanner(System.in);
+    }
+
+    public void insertarNumero() {
+        if (tamaño < numeros.length) {
+            System.out.println("Ingrese un número:");
+            int numero = scanner.nextInt();
+            numeros[tamaño] = numero;
+            tamaño++;
+            System.out.println("Número agregado con éxito.");
+        } else {
+            System.out.println("No se pueden agregar más números. El arreglo está lleno.");
+        }
+    }
+
+    public void mostrarNumeros() {
+        System.out.println("Lista de números:");
+        for (int i = 0; i < tamaño; i++) {
+            System.out.println((i + 1) + ". " + numeros[i]);
+        }
+    }
+
+    public void eliminarNumero() {
+        System.out.println("Ingrese la posición del número a eliminar:");
+        int posicionEliminar = scanner.nextInt();
+        scanner.nextLine();
+        posicionEliminar = posicionEliminar - 1;
+        if (posicionEliminar >= 0 && posicionEliminar < tamaño) {
+            for (int i = posicionEliminar; i < tamaño - 1; i++) {
+                numeros[i] = numeros[i + 1];
+            }
+            tamaño--;
+            System.out.println("Número eliminado con éxito.");
+        } else {
+            System.out.println("Posición inválida. No se pudo eliminar el número.");
+        }
+    }
+
+    public void ordenarBurbuja() {
+        for (int i = 0; i < tamaño - 1; i++) {
+            for (int j = 0; j < tamaño - i - 1; j++) {
+                if (numeros[j] > numeros[j + 1]) {
+                    int temp = numeros[j];
+                    numeros[j] = numeros[j + 1];
+                    numeros[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println("Arreglo ordenado por burbuja: " + Arrays.toString(numeros));
+    }
+
+    public void ordenarSeleccion() {
+        for (int i = 0; i < tamaño - 1; i++) {
+            int indiceMinimo = i;
+            for (int j = i + 1; j < tamaño; j++) {
+                if (numeros[j] < numeros[indiceMinimo]) {
+                    indiceMinimo = j;
+                }
+            }
+            int temp = numeros[indiceMinimo];
+            numeros[indiceMinimo] = numeros[i];
+            numeros[i] = temp;
+        }
+        System.out.println("Arreglo ordenado por selección: " + Arrays.toString(numeros));
+    }
+
+    public void ordenarInsercion() {
+        for (int i = 1; i < tamaño; i++) {
+            int valorActual = numeros[i];
+            int j = i - 1;
+            while (j >= 0 && numeros[j] > valorActual) {
+                numeros[j + 1] = numeros[j];
+                j--;
+            }
+            numeros[j + 1] = valorActual;
+        }
+        System.out.println("Arreglo ordenado por inserción: " + Arrays.toString(numeros));
+    }
+
+    public void ordenarQuicksort(int inicio, int fin) {
+        if (inicio < fin) {
+            int indicePivote = particionar(inicio, fin);
+            ordenarQuicksort(inicio, indicePivote - 1);
+            ordenarQuicksort(indicePivote + 1, fin);
+        }
+    }
+
+    private int particionar(int inicio, int fin) {
+        int pivote = numeros[fin];
+        int i = inicio - 1;
+        for (int j = inicio; j < fin; j++) {
+            if (numeros[j] <= pivote) {
+                i++;
+                int temp = numeros[i];
+                numeros[i] = numeros[j];
+                numeros[j] = temp;
+            }
+        }
+        int temp = numeros[i + 1];
+        numeros[i + 1] = numeros[fin];
+        numeros[fin] = temp;
+        return i + 1;
+    }
+
+    public boolean retornar() {
+        return true;
+    }
+
+    public boolean ejecutar() {
+        boolean retornar = false;
+        int opcion;
+        do {
+            System.out.println("Seleccione una opción:");
+            System.out.println("1. Insertar número");
+            System.out.println("2. Mostrar números");
+            System.out.println("3. Eliminar número");
+            System.out.println("4. Ordenar por burbuja");
+            System.out.println("5. Ordenar por selección");
+            System.out.println("6. Ordenar por inserción");
+            System.out.println("7. Ordenar por Quicksort");
+            System.out.println("0. Retornar");
+
+            opcion = scanner.nextInt();
+            scanner.nextLine(); // Consumir el salto de línea
+
+            switch (opcion) {
+                case 1:
+                    insertarNumero();
+                    break;
+
+                case 2:
+                    mostrarNumeros();
+                    break;
+
+                case 3:
+                    eliminarNumero();
+                    break;
+
+                case 4:
+                    ordenarBurbuja();
+                    break;
+
+                case 5:
+                    ordenarSeleccion();
+                    break;
+
+                case 6:
+                    ordenarInsercion();
+                    break;
+
+                case 7:
+                    ordenarQuicksort(0, tamaño - 1);
+                    System.out.println("Arreglo ordenado por Quicksort: " + Arrays.toString(numeros));
+                    break;
+
+                case 0:
+                    retornar = retornar();
+
+                default:
+                    System.out.println("Opción inválida. Intente nuevamente.");
+                    break;
+            }
+
+            System.out.println();
+        } while (opcion != 0);
+        return retornar;
+    }
 }
--- src/Busqueda/Busqueda.java
+++ src/Busqueda/Busqueda.java
@@ -1,13 +1,161 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Busqueda;
 
-/**
- *
- * @author danny
- */
+import Arreglos.AplicacionArreglo;
+import java.util.Scanner;
+import main.ClaseGenerica;
+
 public class Busqueda {
     
+    private static Scanner scanner = new Scanner(System.in);
+    ClaseGenerica busqueda = new ClaseGenerica();
+
+    public Busqueda() {
+        seteoData();
+    }
+
+    public boolean principal() {
+        boolean retornar = false;
+
+        int opcion;
+        do {
+            mostrarMenu();
+            opcion = leerOpcion();
+
+            switch (opcion) {
+                case 1:
+                    mostrarDefinicion();
+                    break;
+
+                case 2:
+                    mostrarAlgoritmo();
+                    break;
+
+                case 3:
+                    mostrarPruebaDeEscritorio();
+                    break;
+
+                case 4:
+                    mostrarDescripcionDelPrograma();
+                    break;
+
+                case 5:
+                    mostrarAplicacion();
+                    break;
+
+                case 0:
+                    retornar = retornar();
+                    break;
+
+                default:
+                    System.out.println("Opción inválida. Intente nuevamente.");
+                    break;
+            }
+
+            System.out.println();
+        } while (opcion != 0);
+
+        scanner.close();
+
+        return retornar;
+    }
+
+    private static void mostrarMenu() {
+        System.out.println("Seleccione una opción:");
+        System.out.println("1. Definición");
+        System.out.println("2. Algoritmo");
+        System.out.println("3. Prueba de escritorio");
+        System.out.println("4. Descripción del programa");
+        System.out.println("5. Aplicación");
+        System.out.println("0. Retornar");
+    }
+
+    private static int leerOpcion() {
+        System.out.print("Opción: ");
+        return scanner.nextInt();
+    }
+
+    private void mostrarDefinicion() {
+        System.out.println(busqueda.getDefinicion());
+    }
+
+    private void mostrarAlgoritmo() {
+        System.out.println(busqueda.getAlgoritmo());
+    }
+
+    private void mostrarPruebaDeEscritorio() {
+        // AGRGEAR
+    }
+
+    private void mostrarDescripcionDelPrograma() {
+        System.out.println(busqueda.getDescripcionPrograma());
+    }
+
+    private void mostrarAplicacion() {
+        AplicacionBusqueda app = (AplicacionBusqueda) busqueda.getAplicacion();
+        /*boolean retornar = app.ejecutar();
+        if (retornar) {
+            principal();
+        }*/
+    }
+
+    private void seteoData() {
+        busqueda.setDefinicion(
+        "Concepto de Ordenamiento:\n" +
+        "El ordenamiento es un proceso que permite organizar los elementos de una colección de datos en un cierto criterio predefinido. " +
+        "El objetivo principal del ordenamiento es obtener una disposición de los elementos de manera ascendente o descendente, " +
+        "lo que facilita su búsqueda, recuperación y realización de operaciones sobre ellos.\n" +
+        "\n" +
+        "Tipos de Ordenamiento:\n" +
+        "1. Ordenamiento por intercambio directo (burbuja):\n" +
+        "- Consiste en comparar pares de elementos adyacentes y realizar intercambios si están en el orden incorrecto.\n" +
+        "- Repite este proceso hasta que todos los elementos estén ordenados.\n" +
+        "\n" +
+        "2. Ordenamiento por selección:\n" +
+        "- Selecciona repetidamente el elemento más pequeño (o más grande) y lo coloca en su posición correcta.\n" +
+        "- Se va construyendo una sublista ordenada desde el principio hasta el final del arreglo.\n" +
+        "\n" +
+        "3. Ordenamiento por inserción:\n" +
+        "- Recorre el arreglo desde el segundo elemento y lo compara con los elementos anteriores.\n" +
+        "- Si encuentra un elemento menor, lo desplaza hacia la derecha hasta encontrar su posición correcta.\n" +
+        "\n" +
+        "4. Ordenamiento por Quicksort:\n" +
+        "- Divide el arreglo en subarreglos más pequeños basados en un elemento llamado 'pivote'.\n" +
+        "- Luego, reorganiza los elementos de manera que los elementos más pequeños que el pivote estén a su izquierda, y los elementos más grandes estén a su derecha.\n" +
+        "- Repite este proceso recursivamente para los subarreglos hasta que todo el arreglo esté ordenado.");
+        
+        
+        busqueda.setAlgoritmo("Algoritmo de ordenamiento por intercambio directo (burbuja):\n" +
+        "1. Compara pares de elementos adyacentes.\n" +
+        "2. Si están en el orden incorrecto, los intercambia.\n" +
+        "3. Repite este proceso hasta que todos los elementos estén ordenados.\n" +
+        "\n" +
+        "Algoritmo de ordenamiento por selección:\n" +
+        "1. Encuentra el elemento más pequeño (o más grande) en la lista.\n" +
+        "2. Lo coloca en la posición correcta.\n" +
+        "3. Repite este proceso para el resto de elementos, ubicándolos en la sublista ordenada.\n" +
+        "\n" +
+        "Algoritmo de ordenamiento por inserción:\n" +
+        "1. Recorre el arreglo desde el segundo elemento.\n" +
+        "2. Compara el elemento actual con los elementos anteriores.\n" +
+        "3. Si encuentra un elemento menor, lo desplaza hacia la derecha hasta encontrar su posición correcta.\n" +
+        "4. Repite este proceso hasta que todos los elementos estén en su posición correcta.\n" +
+        "\n" +
+        "Algoritmo de ordenamiento por Quicksort:\n" +
+        "1. Elige un elemento pivote del arreglo.\n" +
+        "2. Reorganiza los elementos de manera que los menores que el pivote estén a su izquierda y los mayores estén a su derecha.\n" +
+        "3. Aplica recursivamente este proceso a las sublistas de elementos menores y mayores que el pivote.\n" +
+        "4. Combina las sublistas ordenadas para obtener el arreglo ordenado final.");
+        
+        busqueda.setDescripcionPrograma(
+                "El programa permite agregar nombres de mascotas, dandonos la opcion de consultar mascotas /n"
+                        + "eliminar mascotas y actualizar.");
+        AplicacionArreglo aplicacion = new AplicacionArreglo(5);
+        busqueda.setAplicacion(aplicacion);
+    }
+
+    public boolean retornar() {
+        return true;
+    }
+
 }
